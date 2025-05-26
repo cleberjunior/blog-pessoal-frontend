e3c383674de84733a6cf2f268456474950a5b111
Cleber Farias <jrf.bynho@hotmail.com>
2025-05-26 14:03:56 -0300

    alteração appconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    provideNgxMask({})
   ]
 };
